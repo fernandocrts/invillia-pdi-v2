c6a18e3fc4c047f076f88a1723b623fbb8ad75cc
fernandocrts <fernand.crts@gmail.com>
2019-04-04 17:08:50 -0300

    Versionamento

---
+++ src/main/java/com/invilliatest/rest/webservices/restfulwebservices/person/Personv2.java
@@ -0,0 +1,19 @@
+package com.invilliatest.rest.webservices.restfulwebservices.person;
+
+public class Personv2 {
+	private Name name;
+	
+	public Personv2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+	
+}
